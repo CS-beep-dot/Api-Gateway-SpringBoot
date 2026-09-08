8eb976c9fa593a2356e272ce9a6eac682097d097
CS-beep-dot <romaozgebishvili049@gmail.com>
2026-09-08 17:15:07 +0400

    Repository for roles and users implemented

---
+++ src/main/java/com/roma/apigatewayservice/domain/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.roma.apigatewayservice.domain.repository;
+
+import com.roma.apigatewayservice.domain.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByName(String name);
+}
